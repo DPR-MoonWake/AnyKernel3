6bd779f25d5f10e7f6a1d66d5c35c40f592e4067
Renia in RainyLand <rainyxeon@gmail.com>
2025-10-22 09:18:45 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Shawn the Sheep Kernel @ CUSTOM
+kernel.string=Shaun the Sheep Kernel @ CUSTOM
 do.devicecheck=0
 do.modules=0
 do.systemless=0
