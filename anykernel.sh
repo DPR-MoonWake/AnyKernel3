73edd9fd57a2fe1cff0a36937c0d7c6263e0436d
Renia in RainyLand <rainyxeon@gmail.com>
2026-01-02 21:31:05 +0700

    Update kernel string in anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Shaun the Sheep Kernel @ CUSTOM
+kernel.string=Aeron Phoneix Kernel
 do.devicecheck=0
 do.modules=0
 do.systemless=0
